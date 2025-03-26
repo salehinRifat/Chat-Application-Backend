124c4651bb35a2121713c0094a363bebc60d60ae
Abu Salehin Rifat <salehinrifat2@gmail.com>
2025-03-26 11:10:09 +0600

    Add logout and login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,7 +5,7 @@ import bcrypt from "bcryptjs";
 export const signup = async (req, res) => {
     const { name, email, password } = req.body;
     try {
-        if(!name || !email || !password){
+        if (!name || !email || !password) {
             return res.status(400).json({ message: "All fields are required" });
         }
         if (password.length < 6) {
@@ -23,26 +23,53 @@ export const signup = async (req, res) => {
             password: hashedPassword
         });
         if (newUser) {
-           generateToken(newUser._id,res);
-           await newUser.save();
-             res.status(201).json({ 
-                    _id: newUser._id,
-                    name: newUser.name,
-                    email: newUser.email,
-                    profilePic: newUser.profilePic,
-              });
-         
+            generateToken(newUser._id, res);
+            await newUser.save();
+            res.status(201).json({
+                _id: newUser._id,
+                name: newUser.name,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+            });
+
         } else {
-             res.status(400).json({ message: "Invalid User data" });
+            res.status(400).json({ message: "Invalid User data" });
         }
     } catch (error) {
         console.log(error.message);
         res.status(500).json({ message: "Server Error" });
     }
 };
-export const login = (req, res) => {
-
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        generateToken(user._id, res);
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: "Server Error" });
+    }
 };
 export const logout = (req, res) => {
+    try {
+        res.cookie("jwt", "", { maxAge: 0 });
+        res.status(200).json({ message: "Logged out" });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: "Server Error" });
 
+    }
 };
